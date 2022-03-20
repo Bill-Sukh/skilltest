2a1c9dca02477fe4dc04b5b3bfb764a8adc9d7f8
bill sukh <bill.sukh97@gmail.com>
2022-03-19 23:32:17 -0500

    design 1.0

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// function getStories() {
+//     fetch('https://hacker-news.firebaseio.com/v0/newstories.json')
+//         .then((response) => {
+//             return response.json();
+//         })
+//         .then((myJson) => {
+//             document.getElementById("stories").innerHTML = myJson;
+//         });
+// }
+// getStories();
